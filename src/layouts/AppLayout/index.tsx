ce9b3abe67f5af0d607133d162e1fec52264095a
sondoannam <sondoannam202@gmail.com>
2025-12-22 15:02:17 +0700

    chore(app): :arrow_up: using local pnpm by default & remove POEditor

--- src/layouts/AppLayout/index.tsx
+++ src/layouts/AppLayout/index.tsx
@@ -3,25 +3,25 @@ import { Outlet } from 'react-router-dom';
 
 import { useInitialProfile } from '@/stores/user/useInitialProfile';
 import LoadingSection from '@/components/Common/LoadingSection';
-import { useRequest } from 'ahooks';
-import { getTranslationList } from '@/services/poeditor';
-import { useTranslationStore } from '@/stores/translation/useTranslationStore';
+// import { useRequest } from 'ahooks';
+// import { getTranslationList } from '@/services/poeditor';
+// import { useTranslationStore } from '@/stores/translation/useTranslationStore';
 
 const AppLayout = () => {
   const { loading } = useInitialProfile();
-  const { setTerms } = useTranslationStore((state) => state);
+  // const { setTerms } = useTranslationStore((state) => state);
 
-  useRequest(
-    () =>
-      getTranslationList({
-        language: 'vi',
-      }),
-    {
-      onSuccess: (data) => {
-        setTerms(data.result.terms);
-      },
-    },
-  );
+  // useRequest(
+  //   () =>
+  //     getTranslationList({
+  //       language: 'vi',
+  //     }),
+  //   {
+  //     onSuccess: (data) => {
+  //       setTerms(data.result.terms);
+  //     },
+  //   },
+  // );
 
   if (loading) {
     return <LoadingSection />;
